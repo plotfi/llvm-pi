05fa52271369ff26a5dca960c054e0935fa7f770
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-05-16 22:44:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 apt update
 apt install cmake ninja-build clang clang-format lldb lld gcc g++ vim cargo htop tmux git curl \
             python3-distutils expect net-tools di tig wget \
+            build-essential crossbuild-essential-arm64 \
             libstdc++-8-dev-arm64-cross libstdc++6-arm64-cross libgcc1-arm64-cross libgcc-8-dev-arm64-cross
