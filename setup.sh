1197ba759b940a76c7d16fbe13e7ef9ee40d27cf
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-05-17 23:50:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ ninja -C./llvm-project-build
 ninja -C./llvm-project/build install
 
 
-cmake -B./llvm-test-suite-build -DLLVM_INSTALL_ROOT=/root/toolchain/ -DRPI4_SYSROOT=/root/sysroots/aarch64-linux-gnu \
+cmake -B./llvm-test-suite-build -DLLVM_INSTALL_ROOT=`pwd`/toolchain/ -DRPI4_SYSROOT=`pwd`/sysroots/aarch64-linux-gnu \
       -C./llvm-rpi4/llvm-test-suite-rpi4.cmake -C./llvm-test-suite/cmake/caches/O3.cmake ./llvm-test-suite
 
 make -j16 -C./llvm-test-suite-build VERBOSE=1
