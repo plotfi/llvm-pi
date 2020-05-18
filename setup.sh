a96df25970911ddcb5125515c2f1182a803b4b28
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-05-18 00:02:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+GCC_VERS=10
+
 apt update
 apt install cmake ninja-build clang clang-format lldb lld gcc g++ vim cargo htop tmux git curl \
             python3-distutils expect net-tools di tig wget netcat rsync \
-            build-essential crossbuild-essential-arm64 \
-            libstdc++-8-dev-arm64-cross libstdc++6-arm64-cross libgcc1-arm64-cross libgcc-8-dev-arm64-cross
+            build-essential crossbuild-essential-arm64 libgcc-s1-arm64-cross \
+            libstdc++-$GCC_VERS-dev-arm64-cross libstdc++6-arm64-cross \
+            libgcc1-arm64-cross libgcc-$GCC_VERS-dev-arm64-cross
 
 cd
 mkdir -p sysroot/aarch64-linux-gnu/usr
@@ -17,7 +20,6 @@ cd sysroot/aarch64-linux-gnu/usr
 cp -r -v -L /usr/aarch64-linux-gnu/include /usr/aarch64-linux-gnu/lib .
 
 cd lib
-GCC_VERS=`\ls /usr/lib/gcc-cross/aarch64-linux-gnu/ | tr ' ' '\n' | sort -nr | head -n1`
 cp -r -v -L /usr/lib/gcc-cross/aarch64-linux-gnu/$GCC_VERS/*gcc* .
 cp -r -v -L /usr/lib/gcc-cross/aarch64-linux-gnu/$GCC_VERS/*crt* .
 cp -r -v -L /usr/lib/gcc-cross/aarch64-linux-gnu/$GCC_VERS/libsupc++.a .
