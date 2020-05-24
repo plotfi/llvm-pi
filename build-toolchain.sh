93020da79ba0ca2e6f0abd5633887e95a6011f59
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-05-24 15:49:00 -0400

    Update build-toolchain.sh

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-cd
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_LLD=ON \
-              -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
-              -DCMAKE_ASM_COMPILER=clang \
-              -DCMAKE_INSTALL_PREFIX=/ \
-              -DRPI4_CMAKE_SYSROOT=`pwd`/sysroots/aarch64-linux-gnu \
-              -C./llvm-pi/llvm-rpi4.cmake \
-              -S./llvm-project/llvm \
-              -B./llvm-project-build
 
 ninja -C./llvm-project-build
 DESTDIR=`pwd`/toolchain  ninja -C./llvm-project-build install
