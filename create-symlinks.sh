b465947d7824fb7112b6f9bff242e96c1f477a82
Puyan Lotfi <34139736+plotfi@users.noreply.github.com>
2020-05-24 15:42:34 -0400

    Update create-symlinks.sh

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,5 +2,4 @@ cd
 ln -s ~/.cargo/bin/languageclient ~/LanguageClient-neovim/bin/
 ln -s ~/LanguageClient-neovim ~/.vim/plugins/
 ln -s  ~/llvm-project-build/compile_commands.json ~/llvm-project/
-ln -s ~/LanguageClient-neovim ~/.vim/plugins/
 ln -s /usr/bin/clangd ~/.vim/plugins/
